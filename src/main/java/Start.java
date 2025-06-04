684e27ba0eb5ce8211b0a26a3b305894f94e77c8
davyusow <dayvsonf27@gmail.com>
2025-06-03 22:25:48 -0300

    Adição básica da interface
    - falta tratamento de erros

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -3,6 +3,7 @@ import com.example.atividadeintegrais.view.View;
 public class Start {
     public static void main(String[] args) {
         View view = new View();
-        view.askForInputsSegundaRegraDeSimpsonComMaisDeUmTrioDeSubconjuntos();
+        //view.askForInputsSegundaRegraDeSimpsonComMaisDeUmTrioDeSubconjuntos();
+        view.principal();
     }
 }
--- src/main/java/com/example/atividadeintegrais/view/View.java
+++ src/main/java/com/example/atividadeintegrais/view/View.java
@@ -2,6 +2,7 @@ package com.example.atividadeintegrais.view;
 
 import com.example.atividadeintegrais.calculos.*;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class View {
@@ -139,5 +140,80 @@ public class View {
         System.out.println("\nResultado: " + resultado);
     }
 
+    public void principal(){
+        int opcao,metodo;
+        System.out.println("Deseja aplicar qual método?" +
+                "\n⟶ 1 para o método do trapézio" +
+                "\n⟶ 2 para o método de simpson" +
+                "\n⟶ Opção: ");
+        opcao = lerInteiro(1,2);
+
+        switch (opcao){
+            case 1:
+            System.out.println("Deseja aplicar qual regra?" +
+                    "\n1 ⟶ para o trapézio simples" +
+                    "\n2 ⟶ para o trapézio composto");
+                    metodo = lerInteiro(1,2);
+                    if(metodo == 1){
+                        askForInputsTrapezioSimples();
+                    }else if(metodo == 2){
+                        askForInputsTrapezioComposta();
+                    }
+                break;
+            case 2:
+                System.out.println("Deseja aplicar qual regra?" +
+                        "\n1 ⟶ para a primeira regra de simpson" +
+                        "\n2 ⟶ para a primeira regra de simpson com mais de um par de subintervalos" +
+                        "\n3 ⟶ para a segunda regra de simpson" +
+                        "\n4 ⟶ para a segunda regra de simpson com mais de 3 subintervalos");
+                metodo = lerInteiro(1,4);
+                switch (metodo){
+                    case 1:
+                        askForInputsPrimeiraRegraDeSimpson();
+                        break;
+                    case 2:
+                        askForInputsPrimeiraRegraDeSimpsonComMaisDeUmParDeSubconjuntos();
+                        break;
+                    case 3:
+                        askForInputsSegundaRegraDeSimpson();
+                        break;
+                    case 4:
+                        askForInputsSegundaRegraDeSimpsonComMaisDeUmTrioDeSubconjuntos();
+                        break;
+                }
+            break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
+    }
+
+    private int lerInteiro(int min,int max){
+        int valor = 0;
+        Scanner ler = new Scanner(System.in);
+        do{
+            try {
+                valor = ler.nextInt();
+                return valor;
+            }catch (InputMismatchException e){
+                System.out.println("O valor deve ser um inteiro!");
+                ler.next();
+            }
+        }while (valor < min && valor > max);
+        return valor;
+    }
+    private int lerInteiro(){
+        int valor = 0;
+        Scanner ler = new Scanner(System.in);
+        do{
+            try {
+                valor = ler.nextInt();
+                return valor;
+            }catch (InputMismatchException e){
+                System.out.println("O valor deve ser um inteiro!");
+                ler.next();
+            }
+        }while (true);
+    }
 }
 
